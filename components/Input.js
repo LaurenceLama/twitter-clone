c34fe0ceb1e211f56156b04af20439d7399cd22d
LaurenceLama <laurencelamaFSE@gmail.com>
2023-07-02 19:32:28 +0800

    Emoji mart css not mounting bug

--- components/Input.js
+++ components/Input.js
@@ -1,9 +1,24 @@
-import { XMarkIcon } from "@heroicons/react/24/outline";
-import { useState } from "react";
+import { CalendarIcon, ChartBarIcon, FaceSmileIcon, PhotoIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { useRef, useState } from "react";
+// import "emoji-mart/css/emoji-mart.css";
+import Picker from "@emoji-mart/react";
+import data from "@emoji-mart/data";
 
 function Input() {
-    const [input, setInput] = useState('');
-    const [selectedFile, setSelectedFile] = useState(null)
+  const [input, setInput] = useState("");
+  const [selectedFile, setSelectedFile] = useState(null);
+  const filePickerRef = useRef(null);
+  const [showEmojis, setShowEmojis] = useState(false);
+
+  const addImageToPost = (e) => {}
+
+  const addEmoji = (e) => {
+    let sym = e.unified.split("-");
+    let codesArray = [];
+    sym.forEach((el) => codesArray.push("0x" + el));
+    let emoji = String.fromCodePoint(...codesArray);
+    setInput(input + emoji);
+  };
 
   return (
     <div
@@ -15,7 +30,7 @@ function Input() {
         className="h-11 w-11 rounded-full cursor-pointer"
       />
       <div className="divide-y divide-gray-700 w-full">
-        <div>
+        <div className={``}>
           <textarea
             value={input}
             onChange={(e) => setInput(e.target.value)}
@@ -24,26 +39,68 @@ function Input() {
             className="bg-transparent outline-none text-[#d9d9d9] text-lg placeholder-blue-300 tracking-wide w-full min-h-[50px]"
           />
 
-         {selectedFile && (
-          <div className="relative">
-            <div
-              className="absolute w-8 h-8 bg-[#15181c] hover:bg-[#272c26] bg-opacity-75 rounded-full flex items-center justify-center top-1 left-1 cursor-pointer"
-              onClick={() => setSelectedFile(null)}
-            >
-              <XMarkIcon className="text-white h-5" />
+          {selectedFile && (
+            <div className="relative">
+              <div
+                className="absolute w-8 h-8 bg-[#15181c] hover:bg-[#272c26] bg-opacity-75 rounded-full flex items-center justify-center top-1 left-1 cursor-pointer"
+                onClick={() => setSelectedFile(null)}
+              >
+                <XMarkIcon className="text-white h-5" />
+              </div>
+              <img
+                src={selectedFile}
+                alt=""
+                className="rounded-2xl max-h-80 object-contain"
+              />
             </div>
-            <img
-              src={selectedFile}
-              alt=""
-              className="rounded-2xl max-h-80 object-contain"
-            />
-          </div>
-         )}
+          )}
         </div>
-        
+
+        <div className="flex items-center justify-between pt-2.5">
+          <div className="flex items-center">
+            <div className="icon" onClick={() => filePickerRef.current.click()}>
+              {" "}
+              {/*this filePicker thing finds the ref provided to enable clicking the cool icon instead of the measly 'choose file' that is currently hidden*/}
+              <PhotoIcon className="text-[#1d9bf0] h-[22px]" />
+              <input
+                type="file"
+                ref={filePickerRef}
+                hidden
+                onChange={addImageToPost}
+              />
+            </div>
+
+            <div className="icon rotate-90">
+              <ChartBarIcon className="text-[#1d9bf0] h-[22px]" />
+            </div>
+
+            <div className="icon" onClick={() => setShowEmojis(!showEmojis)}>
+              <FaceSmileIcon className="text-[#1d9bf0] h-[22px]" />
+            </div>
+
+            <div className="icon">
+              <CalendarIcon className="text-[#1d9bf0] h-[22px]" />
+            </div>
+
+            {showEmojis && (
+              <Picker
+                // onSelect={addEmoji}
+                data={data}
+                style={{
+                  position: "absolute",
+                  marginTop: "465px",
+                  marginLeft: -40,
+                  maxWidth: "320px",
+                  borderRadius: "20px",
+                }}
+                theme="dark"
+              />
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
 }
 
-export default Input
+export default Input;
