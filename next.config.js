3dfdf7d27dcca4dc898cd949f68de174625a33fd
LaurenceLama <laurencelamaFSE@gmail.com>
2023-07-12 20:03:13 +0800

    Pending post page

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["rb.gy", "lh3.googleusercontent.com"],
+    domains: [
+      "rb.gy",
+      "lh3.googleusercontent.com",
+      "firebasestorage.googleapis.com",
+    ],
   },
 };
 
